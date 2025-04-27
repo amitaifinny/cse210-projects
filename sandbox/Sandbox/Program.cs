428330a4197d2f6712cdce9c9af312dac41867de
Mckayfinny <mckayfinny@gmail.com>
2025-04-26 19:35:49 -0600

    completed the prep 1 activity.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Input your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your full name is {firstName} {lastName}");
+
+
     }
 }
