f66f78f7e4389b0a11ba1a3170a95a511a652b28
Mckayfinny <mckayfinny@gmail.com>
2025-04-29 10:53:44 -0600

    added a little test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.Write("Input your first name: ");
-        string firstName = Console.ReadLine();
-        Console.Write("Input your last name: ");
-        string lastName = Console.ReadLine();
-        Console.WriteLine($"Your full name is {firstName} {lastName}");
+       int x = 10;
+       if (x==10)
+       {
+        Console.WriteLine("x is 10");
+
+       }
+
+       Console.WriteLine("Hey how are you?");
+
 
 
     }
