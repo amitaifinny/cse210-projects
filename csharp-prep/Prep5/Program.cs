e654f96919d2c73554e0745aee5b8fcded6cbad0
Mckayfinny <mckayfinny@gmail.com>
2025-05-03 23:49:47 -0600

    finished making all the functions for prep 4

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static void PromptUserName(string userName)
+        {
+            Console.WriteLine($"What is your username");
+        }
+
+        static void PromptUserNumber(int favoriteNumber)
+        {
+            Console.WriteLine($"Your favorite number is: {favoriteNumber}");
+        }
+
+        static int SquareNumber(int integer)
+        {
+            int squared = integer * integer;
+            return squared;
+        }
+
+        static void DisplayResult()
+        {
+            Console.WriteLine($"{PromptUserName} the square of your number is {SquareNumber}");
+        }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
     }
 }
