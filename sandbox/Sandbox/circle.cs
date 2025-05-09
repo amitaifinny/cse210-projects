1868854eae9814dcf810cc3aebbb462660f186fd
Mckayfinny <mckayfinny@gmail.com>
2025-05-09 02:23:28 -0600

    completed prepare 2 assignment

--- sandbox/Sandbox/circle.cs
+++ sandbox/Sandbox/circle.cs
@@ -1,43 +1,30 @@
-using System;
-using System.Collections;
-
 // class Program
 // {
     
-    class Program
+    class Circle
     {
-    static void Main(string[] args)
-    {
-        Circle myCircle = new Circle();
-        myCircle.SetRadius(10);
-        Console.WriteLine($"{myCircle.GetRadius()}");
-        Circle myCircle2 = new Circle();
-        myCircle2.SetRadius(20);
-        Console.WriteLine($"{myCircle2.GetRadius()}");
-        Console.WriteLine($"{myCircle.GetArea()}");
-        Console.WriteLine($"{myCircle2.GetArea()}");
-//         private double _radius;
+        private double _radius;
 
-//         public void SetRadius(double radius)
-//         {
-//            if (radius < 0)
-//            {
-//             Console.WriteLine("Error, radius must > 0");
-//            }
-//         //    return;
-//             _radius = radius;
-//         }
+        public void SetRadius(double radius)
+        {
+           if (radius < 0)
+           {
+            Console.WriteLine("Error, radius must > 0");
+           }
+        //    return;
+            _radius = radius;
+        }
 
-//         public double GetRadius()
-//         {
-//             return _radius;
-//         }
+        public double GetRadius()
+        {
+            return _radius;
+        }
 
-//         public double GetArea()
-//         {
-//             return Math.PI * _radius * _radius;
-//         }
-//     }
+        public double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
         // Console.WriteLine("Bonjour tout le monde");
         // List<int> myNumbers = new List<int>();
         // myNumbers.Add(99);
@@ -81,10 +68,9 @@ using System.Collections;
 
         // } while(!done);
 
-    }
+    // }
 
     // static double AddNumbers(double a, double b)
     // {
     //     return a + b;
     // }
-    }
