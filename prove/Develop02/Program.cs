e426f84615c3bf52800bc29624fd8061f654ff14
Mckayfinny <mckayfinny@gmail.com>
2025-05-17 00:09:56 -0600

    added comment about stretch challenge

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -11,3 +11,5 @@ class Program
         menu.MakeMenu();
     }
 }
+
+//For the stretch challenge, I exceeded the requirements by adding a unique word generator class. I added a new option to the menu. The user can select 2 to see a unique word with its definition. This could help the user to spice up their journal entry and use a new, unique word if they would like. Or it could just be a fun thing they could do, if they wanted to deepen their vocabulary.
