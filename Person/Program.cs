f055bdd6a8fac6065d0bd88c0fd3199ca9987e9e
Mckayfinny <mckayfinny@gmail.com>
2025-06-06 16:45:47 -0600

    completed inheritance example

--- Person/Program.cs
+++ Person/Program.cs
@@ -2,6 +2,16 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("bob");
+        Person person1 = new Person("bob", "Lemhi", 20);
+        Console.WriteLine(person1.GetPersonInformation());
+
+        PoliceMan newPoliceMan = new PoliceMan("Betty", "crock", 79, "gun");
+
+        Console.WriteLine(newPoliceMan.GetPersonInformation());
+        Console.WriteLine(newPoliceMan.GetPoliceManInformation());
+
+        Doctor myDoctor = new Doctor("ben", "kevin", 1, "knife");
+        Console.WriteLine(myDoctor.GetDoctorInformation());
     }
 }
+
