0d46ed2502d4551b3d00a365a84084693ba1e199
Mckayfinny <mckayfinny@gmail.com>
2025-06-06 17:37:03 -0600

    finished all classes and completed program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Bob Kevin", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Senor Wooly", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssingment assignment3 = new WritingAssingment("Sour Strips", "Euroean History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,16 @@
+public class WritingAssingment : Assignment
+{
+    string _title;
+
+    public WritingAssingment(string studentName, string topic, string title)
+    : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {GetStudentName()}";
+    }
+
+}
