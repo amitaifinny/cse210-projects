1f7db281997be8675ba75fdc1d6e13da4dbd58af
Mckayfinny <mckayfinny@gmail.com>
2025-06-26 18:05:15 -0600

    finished prepare assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 5.0);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("red", 3.0, 4.3);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("black", 2.75);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has this area: {area}. ");
+        }
+
+
+
+       
     }
 }
