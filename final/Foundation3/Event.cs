0d745683a944622d597d178b560d0e96992c81ed
Mckayfinny <mckayfinny@gmail.com>
2025-07-21 15:51:54 -0600

    finished event and lecture class

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,75 @@
+using System.Reflection;
+
+public abstract class Event
+{
+    private string _title;
+    private string _description;
+    private string _date;
+    private string _time;
+    protected Address _address;
+
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+
+    public abstract string ListShortDetails();
+    public string ListStandardDetails()
+    {
+        return $"{_title}\n{_description}\nWhen: {_date} at {_time}\nLocation: {_address.GetAddressRepresentation()}";
+    }
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+
+    public void SetTitle(string title)
+    {
+        _title = title;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public void SetDate(string date)
+    {
+        _date = date;
+    }
+    public string GetTime()
+    {
+        return _time;
+    }
+
+    public void SetTime(string time)
+    {
+        _time = time;
+    }
+
+    public Address GetAddress()
+    {
+        return _address;
+    }
+
+    public void SetAddress(Address address)
+    {
+        _address = address;
+    }
+
+}
---
+++ final/Foundation3/Lecture.cs
@@ -0,0 +1,23 @@
+public class Lecture : Event
+{
+    private string _speaker;
+    private int _capacity;
+
+
+    public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity)
+    : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
+
+    public string DisplayLectureDescription()
+    {
+        return $"{ListStandardDetails()}\nEvent: Lecture\nSpeaker: {_speaker}\nCapacity: {_capacity}";
+    }
+
+    public override string ListShortDetails()
+    {
+        return $"Event: Lecture\n{GetTitle()}\n{GetDate()}";
+    }
+}
