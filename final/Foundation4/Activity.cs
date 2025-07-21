12208a08ade063d6257bf8604372210db42ec286
Mckayfinny <mckayfinny@gmail.com>
2025-07-21 17:21:33 -0600

    finished activity class

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,43 @@
+public abstract class Activity
+{
+    private string _date;
+    private double _lengthInMinutes;
+
+    public Activity(string date, double lengthInMinutes)
+    {
+        _date = date;
+        _lengthInMinutes = lengthInMinutes;
+    }
+
+    public abstract double CalculateDistance();
+    public abstract double CalculateSpeed();
+    public abstract double CalculatePace();
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public void SetDate(string date)
+    {
+        _date = date;
+    }
+
+    public double GetLengthInMinutes()
+    {
+        return _lengthInMinutes;
+    }
+
+    public void SetLength(double lengthInMinutes)
+    {
+        _lengthInMinutes = lengthInMinutes;
+    }
+
+    public virtual string GetSummary()
+    {
+        string activityType = this.GetType().Name;
+        return $"{_date} {activityType}({_lengthInMinutes})- Distance: {CalculateDistance():F1} miles, Speed {CalculateSpeed():F1} mph, Pace: {CalculatePace():F1} min per mile.";
+    }
+    
+
+}
